c89f6e1d2ea53426e51d5b070ec254c269ac82d7
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-14 13:14:54 -0300

    Creating a Form

--- src/app/movies/register-movies/register-movies.component.ts
+++ src/app/movies/register-movies/register-movies.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "dio-register-movies",
@@ -8,15 +8,31 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 })
 export class RegisterMoviesComponent implements OnInit {
 
-  options: FormGroup;
+  register: FormGroup;
 
   constructor(private fb: FormBuilder) {
-  };
+  }
 
-  ngOnInit() {
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: "auto",
+  ngOnInit(): void {
+    this.register = this.fb.group({
+      title: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlImage: ["", [Validators.minLength(10)]],
+      releaseDate: ["", [Validators.required]],
+      description: [""],
+      rating: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ["", [Validators.minLength(10)]],
+      genre: ["", [Validators.required]]
     });
-  };
-};
+  }
+
+  save(): void {
+    if (this.register.invalid) {
+      return;
+    }
+    alert("SUCCESS!!\n\n" + JSON.stringify(this.register.value, null, 4));
+  }
+
+  resetForm(): void {
+    this.register.reset();
+  }
+}
